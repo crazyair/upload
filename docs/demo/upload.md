a53142547d913f75b810b069f9c0e6ff6df986d2
叶枫 <645381995@qq.com>
2024-05-20 09:35:04 +0800

    feat: upload

---
+++ docs/demo/upload.md
@@ -0,0 +1,8 @@
+---
+title: upload
+nav:
+  title: Demo
+  path: /demo
+---
+
+<code src="../examples/upload.tsx"/></code>
---
+++ docs/examples/upload.tsx
@@ -0,0 +1,39 @@
+import Upload, { UploadProps } from 'rc-upload';
+
+const props: UploadProps = {
+  action: () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('/upload.do');
+      }, 2000);
+    });
+  },
+  multiple: true,
+  onStart(file) {
+    console.log('onStart', file, file.name);
+  },
+  onSuccess(ret) {
+    console.log('onSuccess', ret);
+  },
+  onError(err) {
+    console.log('onError', err);
+  },
+};
+
+const Test = () => {
+  return (
+    <div
+      style={{
+        margin: 100,
+      }}
+    >
+      <div>
+        <Upload {...props}>
+          <a>开始上传</a>
+        </Upload>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
